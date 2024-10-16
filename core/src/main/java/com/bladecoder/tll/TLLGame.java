9a7c8fdb5f9b7d785cf2992c06744db0d2f92237
Rafael García <bladecoder@gmail.com>
2024-10-16 21:33:03 +0200

    Postprocessor Crt effect

--- core/src/main/java/com/bladecoder/tll/TLLGame.java
+++ core/src/main/java/com/bladecoder/tll/TLLGame.java
@@ -7,6 +7,13 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.bitfire.postprocessing.PostProcessor;
+import com.bitfire.postprocessing.effects.Bloom;
+import com.bitfire.postprocessing.effects.CrtMonitor;
+import com.bitfire.postprocessing.effects.Curvature;
+import com.bitfire.postprocessing.filters.Combine;
+import com.bitfire.postprocessing.filters.CrtScreen;
+import com.bitfire.utils.ShaderLoader;
 import com.bladecoder.tll.blocks.BlocksScreen;
 import com.bladecoder.tll.ui.BladeSkin;
 import com.bladecoder.tll.util.EngineLogger;
@@ -19,6 +26,9 @@ public class TLLGame extends Game {
     private MainMenuScreen menuScreen;
 
     private BladeSkin skin;
+
+    private PostProcessor postProcessor;
+    private CrtMonitor crt;
 
     public BladeSkin getSkin() {
         return skin;
@@ -54,10 +64,21 @@ public class TLLGame extends Game {
     }
 
     @Override
+    public void render () {
+        float delta = Gdx.graphics.getDeltaTime();
+        crt.setTime(delta);
+        postProcessor.capture();
+        super.render();
+        postProcessor.render();
+    }
+
+    @Override
     public void resume() {
         if(Gdx.app.getType() == Application.ApplicationType.Android) {
             loadAssets();
         }
+
+        postProcessor.rebind();
 
         super.resume();
     }
@@ -66,11 +87,41 @@ public class TLLGame extends Game {
     public void dispose() {
         skin.dispose();
         RectangleRenderer.dispose();
+        postProcessor.dispose();
     }
 
     private void loadAssets() {
         FileHandle skinFile = Gdx.files.internal(SKIN_FILENAME);
         TextureAtlas atlas = new TextureAtlas(Gdx.files.internal(SKIN_FILENAME.substring(0, SKIN_FILENAME.lastIndexOf('.')) + ".atlas"));
         skin = new BladeSkin(skinFile, atlas);
+        setupPostprocessor();
+    }
+
+    private void setupPostprocessor() {
+        ShaderLoader.BasePath = "shaders/";
+        //boolean isDesktop = Gdx.app.getType() == Application.ApplicationType.Desktop;
+        postProcessor = new PostProcessor( false, false, true );
+
+        Curvature curvature = new Curvature();
+        curvature.setDistortion(0.1f);
+        postProcessor.addEffect( curvature );
+
+//        Vignette vignette = new Vignette( Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false );
+//        vignette.setIntensity(0.5f);
+//        postProcessor.addEffect( vignette );
+
+        int effects = CrtScreen.Effect.TweakContrast.v | CrtScreen.Effect.PhosphorVibrance.v | CrtScreen.Effect.Scanlines.v | CrtScreen.Effect.Tint.v | CrtScreen.Effect.Vignette.v ;
+        crt = new CrtMonitor( Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true, true, CrtScreen.RgbMode.ChromaticAberrations, effects );
+        crt.setDistortion(.1f);
+        Combine combine = crt.getCombinePass();
+        combine.setSource1Intensity( 0f );
+        combine.setSource2Intensity( 1f );
+        combine.setSource1Saturation( 0f );
+        combine.setSource2Saturation( 1f );
+
+        postProcessor.addEffect( crt );
+
+        Bloom bloom = new Bloom( (int)(Gdx.graphics.getWidth() * 0.25f), (int)(Gdx.graphics.getHeight() * 0.25f) );
+        postProcessor.addEffect( bloom );
     }
 }
